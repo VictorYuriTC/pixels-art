ea5319b0d3f27c64bccc28cf5e8b7cceb76197c6
Victor Yuri <victoryuritc@yahoo.com>
2022-04-25 18:56:52 -0300

    Adição de laço for para procurar a cor escolhida. Desafio 7.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-let black = document.getElementById('color_black');
+const black = document.getElementById('color-black');
+const red = document.getElementById('color-red');
+const pink = document.getElementById('color-pink');
+const purple = document.getElementById('color-purple');
+
+const color = document.querySelectorAll('.color');
+const pixel = document.querySelectorAll('.pixel');
+const reset = document.getElementById('clear-board');
+
+window.onload = function startPixelArt() {
+  return black.classList.toggle('selected');
+};
+
+const colors = [black, red, pink, purple];
+
+color.onclick = function searchSelectedColor() {
+  black.classList.remove('selected');
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].onclick = function changeSelectedColor() {
+      return colors[i].classList.toggle('selected');
+    };
+  }
+};
+
+reset.onclick = function resetBoard() {
+  pixel.style.backgroundColor = 'white';
+};
