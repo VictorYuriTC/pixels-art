a8c5cdbe7b8fbd417010b0a8c60a2bc3b0a94a29
Victor Yuri <victoryuritc@yahoo.com>
2022-04-26 01:23:37 -0300

    Adicionando a função para selecionar as cores desejadas. Desafio 7.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const black = document.getElementById('color-black');
 const red = document.getElementById('color-red');
 const pink = document.getElementById('color-pink');
 const purple = document.getElementById('color-purple');
-
-const color = document.getElementsByClassName('color');
 const board = document.getElementById('pixel-board');
+
+/* A loop for created to generate pixels using appendChild */
 
 for (let i = 0; i < 25; i += 1) {
   const newPixel = document.createElement('li');
@@ -12,27 +12,55 @@ for (let i = 0; i < 25; i += 1) {
   board.appendChild(newPixel);
 }
 
+/* Starting the pixel art project with the color black selected */
+
 window.onload = function startPixelArt() {
-  return black.classList.toggle('selected');
+  black.classList.toggle('selected');
 };
 
-const colors = [black, red, pink, purple];
+/* Function to change the selected color */
 
-color.onclick = function searchSelectedColor() {
+function changeColor(event) {
   black.classList.remove('selected');
-  for (let i = 0; i < colors.length; i += 1) {
-    colors[i].onclick = function changeSelectedColor() {
-      return colors[i].classList.toggle('selected');
-    };
-  }
-};
+  red.classList.remove('selected');
+  pink.classList.remove('selected');
+  purple.classList.remove('selected');
+  event.target.classList.add('selected');
+}
 
-const reset = document.getElementById('clear-board');
+document.querySelectorAll('.color').forEach((item) => {
+  item.addEventListener('click', changeColor);
+});
+
+/* Lines to create the reset button */
+
+const reset = document.createElement('button');
+reset.id = 'clear-board';
+reset.innerText = 'Limpar';
+document.body.insertBefore(reset, board);
+
+/* Lines to change the button style */
 
 reset.onmouseover = function buttonIce() {
   reset.style.background = '#ace2ef';
+  reset.style.cursor = 'pointer';
 };
 
 reset.onmouseleave = function buttonWhite() {
   reset.style.background = 'white';
 };
+
+/* Adding an event listener to the reset button */
+
+function resetColors() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+
+const clearBoard = document.querySelector('#clear-board');
+function clear() {
+  clearBoard.addEventListener('click', resetColors);
+}
+clear();
