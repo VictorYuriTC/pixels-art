05a7c9d582a25523ab021a3b90b9a67d97822547
Victor Yuri <victoryuritc@yahoo.com>
2022-04-28 17:57:22 -0300

    Adição do bootstrap; adição de input e botão para criar board com outro tamanho.

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-/* Generating board */
+/* Generating board and changing pixels' cursor style */
 
 function generateBoard() {
+  const board = document.getElementById('pixel-board');
   const boardLines = 5;
   for (let i = 0; i < boardLines * boardLines; i += 1) {
-    const board = document.getElementById('pixel-board');
     const generatePixel = document.createElement('div');
     generatePixel.classList = 'pixel';
     generatePixel.style.backgroundColor = 'white';
     board.appendChild(generatePixel);
   }
+  const generateInput = document.createElement('input');
+  generateInput.id = 'board-size';
+  generateInput.placeholder = 'Change board size';
+  generateInput.style.width = '140px';
+  document.body.insertBefore(generateInput, board);
+  const generateButton = document.createElement('button');
+  generateButton.id = 'generate-board';
+  generateButton.innerText = 'VQV';
+  document.body.insertBefore(generateButton, board);
 }
 
 /* Defining "black" as standard selected color */
@@ -52,7 +61,7 @@ function paintPixelClicking() {
   }
 }
 
-/* Adding a button to clear the entire board */
+/* Cleaning the board with a button and changing its style */
 
 function createButton() {
   const board = document.getElementById('pixel-board');
@@ -60,24 +69,25 @@ function createButton() {
   generateButton.id = 'clear-board';
   generateButton.innerText = 'Limpar';
   generateButton.style.display = 'block';
+  generateButton.style.padding = '5px';
   document.body.insertBefore(generateButton, board);
-/*   function changeButtonStyle() {
+  function changeButtonStyle() {
     const button = document.getElementById('clear-board');
     button.onmouseover = function buttonColorIce() {
       button.style.backgroundColor = '#c6dff3';
+      button.style.cursor = 'pointer';
     };
-    button.onmouseleave = function buttonColorWhite() {
+    button.onmouseout = function buttonColorWhite() {
       button.style.backgroundColor = 'white';
     };
   }
-  changeButtonStyle(); */
+  changeButtonStyle();
 }
 
 function resetButton() {
   const pixels = document.querySelectorAll('.pixel');
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
-    document.body.style.backgroundColor = 'white';
   }
 }
 
@@ -85,15 +95,6 @@ function resetButtonClicking() {
   const button = document.getElementById('clear-board');
   button.addEventListener('click', resetButton);
 }
-
-/* Changing the cursor style */
-
-/* function changeCursorStyle() {
-  const button = document.getElementById('clear-board');
-  button.onmouseover = function cursorStyle() {
-    button.style.cursor = 'pointer';
-  };
-} */
 
 /* Loading webpage and invoking functions */
 
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./script.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
