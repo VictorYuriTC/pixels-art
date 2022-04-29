b695864d3ff8ab2392cde13e8e6e4c302924d879
Victor Yuri <victoryuritc@yahoo.com>
2022-04-29 19:59:25 -0300

    Adição de botão para inserir novas cores à paleta de cores sem que as cores estabelecidas sejam retirada. Limite de 200 cores à paleta. Tentativa de fazer com que o usuário escolha quantas cores adicionar à paleta. 250 linhas de código. Fim do projeto.

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 /* Declaring and changing the style of main elements */
 
-/* const bodyStyle = document.body.style;
-bodyStyle.alignItems = 'center';
-bodyStyle.background = '#f5f5f5';
-bodyStyle.display = 'flex';
-bodyStyle.height = '100%';
-bodyStyle.justifyContent = 'center';
-bodyStyle.margin = '0';
-bodyStyle.padding = '0'; */
 const board = document.querySelector('#pixel-board');
 const boardStyle = board.style;
 boardStyle.display = 'block';
@@ -22,40 +14,8 @@ function generateBoard(boardLines) {
   for (let i = 0; i < boardLines * boardLines; i += 1) {
     const generatePixel = document.createElement('div');
     generatePixel.classList = 'pixel';
-    generatePixel.style.backgroundColor = 'white';
-    generatePixel.style.border = 'black solid 1px';
-    generatePixel.style.cursor = 'cell';
-    generatePixel.style.display = 'inline-block';
-    generatePixel.style.height = '2.5em';
-    generatePixel.style.width = '2.5em';
     board.appendChild(generatePixel);
   }
-}
-
-/* Removing board in order to create a new board */
-
-function removeBoard() {
-  const pixels = document.querySelectorAll('.pixel');
-  for (let i = 0; i < pixels.length; i += 1) {
-    pixels[i].remove();
-  }
-}
-
-/* Removing and selecting a color */
-
-function selectColor(selectedColor) {
-  const color = document.querySelectorAll('.color');
-  color.forEach((item) => {
-    item.classList.remove('selected');
-  });
-  selectedColor.target.classList.add('selected');
-}
-
-function selectColorClicking() {
-  const color = document.querySelectorAll('.color');
-  color.forEach((item) => {
-    item.addEventListener('click', selectColor);
-  });
 }
 
 /* Painting pixels */
@@ -75,14 +35,85 @@ function paintPixelClicking() {
   }
 }
 
+/* Removing board in order to create a new board */
+
+function removeBoard() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].remove();
+  }
+}
+
+/* Creating an input bar to generate a new board with
+a chosen number between 5 and 50, including 5 and 50 */
+
+function selectNewBoardSize() {
+  const generateBoardInput = document.createElement('input');
+  generateBoardInput.id = 'board-size';
+  generateBoardInput.min = 1;
+  generateBoardInput.max = 50;
+  generateBoardInput.placeholder = 'Alterar tamanho do quadro';
+  generateBoardInput.type = 'number';
+  generateBoardInput.style.display = 'inline-block';
+  generateBoardInput.style.padding = '5px';
+  generateBoardInput.style.width = '195px';
+  document.body.insertBefore(generateBoardInput, board);
+  const generateBoardButton = document.createElement('button');
+  generateBoardButton.id = 'generate-board';
+  generateBoardButton.innerText = 'VQV';
+  generateBoardButton.style.padding = '5px';
+  document.body.insertBefore(generateBoardButton, board);
+}
+
+/* Generating a new board with selectBoardSize's input */
+
+function generateNewBoard() {
+  const boardSize = document.querySelector('#board-size').value;
+  boardSize.min = 5;
+  boardSize.max = 50;
+  if (boardSize === ''/*  || boardSize == 0 */) {
+    alert('Board inválido!');
+  } else if (boardSize < 5) {
+    removeBoard();
+    generateBoard(5);
+  } else if (boardSize > 50) {
+    removeBoard();
+    generateBoard(50);
+  } else {
+    removeBoard();
+    generateBoard(boardSize);
+  }
+  paintPixelClicking();
+}
+
+function generateNewBoardClicking() {
+  const boardButton = document.querySelector('#generate-board');
+  boardButton.addEventListener('click', generateNewBoard);
+}
+
+/* Removing and selecting a color */
+
+function selectColor(selectedColor) {
+  const color = document.querySelectorAll('.color');
+  color.forEach((item) => {
+    item.classList.remove('selected');
+  });
+  selectedColor.target.classList.add('selected');
+}
+
+function selectColorClicking() {
+  const color = document.querySelectorAll('.color');
+  color.forEach((item) => {
+    item.addEventListener('click', selectColor);
+  });
+}
+
 /* Cleaning the board with a button and changing its style */
 
 function createClearButton() {
   const generateClearButton = document.createElement('button');
   generateClearButton.id = 'clear-board';
   generateClearButton.innerText = 'Limpar';
-  generateClearButton.style.display = 'block';
-  generateClearButton.style.padding = '5px';
   document.body.insertBefore(generateClearButton, board);
   function changeClearButtonStyle() {
     generateClearButton.onmouseover = function buttonColorIce() {
@@ -108,62 +139,7 @@ function clearBoardClicking() {
   clearButton.addEventListener('click', clearBoard);
 }
 
-/* Creating an input bar to generate a new board with
-a chosen number between 5 and 50, including 5 and 50 */
-
-function selectNewBoardSize() {
-  const generateInput = document.createElement('input');
-  generateInput.id = 'board-size';
-  generateInput.min = 1;
-  generateInput.max = 50;
-  generateInput.placeholder = 'Alterar tamanho do quadro';
-  generateInput.type = 'number';
-  generateInput.style.display = 'inline-block';
-  generateInput.style.padding = '5px';
-  generateInput.style.width = '180px';
-  document.body.insertBefore(generateInput, board);
-  const generateBoardButton = document.createElement('button');
-  generateBoardButton.id = 'generate-board';
-  generateBoardButton.innerText = 'VQV';
-  generateBoardButton.style.padding = '5px';
-  document.body.insertBefore(generateBoardButton, board);
-}
-selectNewBoardSize();
-
-/* Generating a new board with selectBoardSize's input */
-
-function generateNewBoard() {
-  const boardSize = document.querySelector('#board-size').value;
-  boardSize.min = 5;
-  boardSize.max = 50;
-  const newBoard = Number(boardSize);
-  if (boardSize === ''/*  || boardSize == 0 */) {
-    alert('Board inválido!');
-  } else if (boardSize < 5) {
-    removeBoard();
-    generateBoard(5);
-  } else if (boardSize > 50) {
-    removeBoard();
-    generateBoard(50);
-  } else {
-    removeBoard();
-    generateBoard(newBoard);
-  }
-  paintPixelClicking();
-}
-
-function generateNewBoardClicking() {
-  const boardButton = document.querySelector('#generate-board');
-  boardButton.addEventListener('click', generateNewBoard);
-}
-
-/* Chosing 3 random colors to be shown in the color palette
-along with the black color (only using numbers) */
-
-/* function to be added */
-
-/* Chosing 3 random colors to be shown in the color palette
-along with the black color (using numbers and letters */
+/* Selecting black as the standard color */
 
 function generateBlackColor() {
   const insertBlackColor = document.createElement('div');
@@ -173,9 +149,13 @@ function generateBlackColor() {
   colorPalette.appendChild(insertBlackColor);
 }
 
+/* Choosing 3 random colors to be shown in the color palette along
+with the black color using hexcolor numbers and letters (using
+only numbers is also possible) */
+
 /* Source: https://www.codegrepper.com/code-examples/javascript/random+letter+in+javascript+generator */
 function generateRandomColor() {
-  let randomColor = `${'#'}`;
+  let randomColor = '#';
   const hexCharacters = '0123456789ABCDEF';
   for (let i = 0; i < 6; i += 1) {
     randomColor += hexCharacters.charAt(Math.random() * hexCharacters.length);
@@ -183,8 +163,10 @@ function generateRandomColor() {
   return randomColor;
 }
 
-function changeRandomColor() {
-  for (let i = 0; i < 3; i += 1) {
+function changeRandomColor(numberColors) {
+  let standardNumberColors = numberColors;
+  standardNumberColors = 3;
+  for (let i = 0; i < standardNumberColors; i += 1) {
     const insertRandomColor = document.createElement('div');
     insertRandomColor.classList.add('color');
     insertRandomColor.classList.add('created-color');
@@ -205,27 +187,61 @@ function removeRandomColor() {
 }
 
 function changeRandomColorClicking() {
-  const colorButton = document.createElement('button');
-  colorButton.id = 'color-button';
-  colorButton.innerText = 'Gerar paleta de cores aleatoriamente';
-  colorButton.style.display = 'block';
-  colorButton.style.padding = '5px';
+  const generateChangeColorButton = document.createElement('button');
+  generateChangeColorButton.id = 'color-button';
+  generateChangeColorButton.innerText = 'Gerar paleta de cores aleatoriamente';
+  generateChangeColorButton.style.display = 'block';
+  generateChangeColorButton.style.padding = '5px';
   const boardSize = document.querySelector('#board-size');
-  document.body.insertBefore(colorButton, boardSize);
-  colorButton.addEventListener('click', removeRandomColor);
-  colorButton.addEventListener('click', changeRandomColor);
-  colorButton.addEventListener('click', selectColorClicking);
+  document.body.insertBefore(generateChangeColorButton, boardSize);
+  generateChangeColorButton.addEventListener('click', removeRandomColor);
+  generateChangeColorButton.addEventListener('click', changeRandomColor);
+  generateChangeColorButton.addEventListener('click', selectColorClicking);
+}
+
+/* Adding new colors to the palette without removing
+any color and limiting the amount of colors to 200 */
+
+function randomColorsAmount() {
+  const generateAddColorInput = document.createElement('input');
+  generateAddColorInput.id = 'add-color-input';
+  generateAddColorInput.placeholder = 'Quantidade de novas cores';
+  generateAddColorInput.type = 'number';
+  generateAddColorInput.value = '1';
+  document.body.insertBefore(generateAddColorInput, colorPalette);
+  const generateAddColorButton = document.createElement('button');
+  generateAddColorButton.id = 'add-color-button';
+  generateAddColorButton.innerHTML = 'Adicionar novas cores à paleta atual';
+  document.body.insertBefore(generateAddColorButton, colorPalette);
+}
+
+function limitColorsAmount() {
+  const insertedRandomColor = document.querySelectorAll('.created-color');
+  if (insertedRandomColor.length > 200) {
+    for (let i = 201; i <= insertedRandomColor.length; i += 1) {
+      insertedRandomColor[i].remove();
+    }
+  }
+}
+
+function randomColorsAmoutClicking() {
+  const addColorButton = document.querySelector('#add-color-button');
+  addColorButton.addEventListener('click', changeRandomColor);
+  addColorButton.addEventListener('click', limitColorsAmount);
 }
 
 /* Loading webpage and invoking functions */
 
 window.onload = function startingPixelArt() {
+  selectNewBoardSize();
   createClearButton();
   generateBlackColor();
   generateBoard(5);
   generateNewBoardClicking();
   changeRandomColor();
   changeRandomColorClicking();
+  randomColorsAmount();
+  randomColorsAmoutClicking();
   paintPixelClicking();
   clearBoardClicking();
   selectColorClicking();
